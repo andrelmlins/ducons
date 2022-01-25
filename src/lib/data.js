fcf712bab5db7ffa0a0b366f54bb9a76190648dc
DutraGames <gabrieldutraamarante@gmail.com>
2022-01-25 20:59:43 -0300

    pack icons

---
+++ src/lib/data.js
@@ -0,0 +1,9 @@
+import GooglePlay from './icons/google-play.svg'
+import Bookmark from './icons/bookmark.svg'
+import BookmarkOutline from './icons/bookmark-outline.svg'
+
+export const dados = {
+    "google-play": GooglePlay,
+    "bookmark": Bookmark,
+    "bookmark-outline": BookmarkOutline
+}
