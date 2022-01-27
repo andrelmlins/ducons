fff171ccf7432ea70fa26e9a27faa3518b9543a0
DutraGames <gabrieldutraamarante@gmail.com>
2022-01-26 23:18:34 -0300

    add plays icons

--- src/lib/data.js
+++ src/lib/data.js
@@ -5,6 +5,8 @@ import AddCircle from './icons/add-circle.svg'
 import AddCircleOutline from './icons/add-circle-outline.svg'
 import Star from './icons/star.svg'
 import StarOutline from './icons/star-outline.svg'
+import Play from './icons/play.svg'
+import PlayOutline from './icons/play-outline.svg'
 
 export const dados = {
     "google-play": GooglePlay,
@@ -14,4 +16,6 @@ export const dados = {
     "add-circle-outline": AddCircleOutline,
     "star": Star,
     "star-outline": StarOutline,
+    "play": Play,
+    "play-outline": PlayOutline,
 }
